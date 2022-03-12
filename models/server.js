e772b9708045eefd27cde7350a155314a637e1cc
Samuel <shamuperez93@gmail.com>
2022-03-12 17:44:58 -0500

    RestServer

---
+++ models/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const cors = require('cors');
+class Server {
+    constructor(){
+        this.app = express();
+        this.port = process.env.PORT;
+        this.usuariosPath = '/api/usuarios';
+
+        // Middlewares
+        this.middleware();
+
+        // Rutas de la app
+        this.routes();
+    }
+
+    middleware(){
+        // CORS
+        this.app.use(cors());
+
+        // Parseo y lectura del body 
+        this.app.use( express.json());
+
+        // Directorio público
+        this.app .use( express.static('public') );
+        
+    }
+
+    routes(){
+        this.app.use(this.usuariosPath, require('../routes/usuarios'));
+    }
+
+    listen(){
+        this.app.listen( this.port , () => {
+            console.log(`Servidor corriendo en el puetro ${this.port}`);
+        });
+    }
+
+}
+
+
+module.exports = Server;
